219a593517d58b0b881619d2c17597d039cebd52
kavindu09611 <kavinduwickramasinghe1920@gmail.com>
2023-03-07 15:09:22 +0530

    frontend

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view(uri:'/',view:'index')->name(name:'home');
+Route::view(uri:'/about',view:'about')->name(name:'about');
+Route::view(uri:'/contact',view:'contact')->name(name:'contact');
+
+
+
 
 
